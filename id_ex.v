f41135c39824d14772f97dffcb7fa1d7c503ae8d
Lien881119 <Lien881119@gmail.com>
2022-12-29 00:47:58 +0800

    Update id_ex.v

--- id_ex.v
+++ id_ex.v
@@ -18,5 +18,21 @@ module id_ex (
     output reg[`RegAddrBus] id_wd,
     output reg ex_wreg
 );
-    
+    always @(posedge clk ) begin
+        if (rst == `RstEnable) begin
+            ex_aluop <= `EXE_NOP_OP;
+            ex_alusel <= `EXE_RES_NOP;
+            ex_reg1 <= `ZeroWord;
+            ex_reg2 <= `ZeroWord;
+            ex_wd <= `NOPRegAddr;
+            ex_wreg <= `WriteDisable;
+        end else begin
+            ex_aluop <= id_aluop;
+            ex_alusel <= id_alusel;
+            ex_reg1 <= id_reg1;
+            ex_reg2 <= id_reg2;
+            ex_wd <= id_wd;
+            ex_wreg <= id_wreg;
+        end
+    end
 endmodule
