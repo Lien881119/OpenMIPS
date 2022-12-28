b423bb581af3e98d779c7ad1175315f1903073d5
Lien881119 <Lien881119@gmail.com>
2022-12-29 00:24:20 +0800

    Update id_ex.v

--- id_ex.v
+++ id_ex.v
@@ -6,6 +6,17 @@ module id_ex (
 
     input wire[`AluOpBus] id_aluop,
     input wire[`AluSelBus] id_alusel,
+    input wire[`RegBus] id_reg1,
+    input wire[`RegBus] id_reg2,
+    input wire[`RegAddrBus] id_wd,
+    input wire id_wreg,
+
+    output reg[`AluOpBus] ex_aluop,
+    output reg[`AluSelBus] ex_alusel,
+    output reg[`RegBus] ex_reg1,
+    output reg[`RegBus] ex_reg2,
+    output reg[`RegAddrBus] id_wd,
+    output reg ex_wreg
 );
     
 endmodule
