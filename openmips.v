c010ce0b9fd63d910cba825f586a26fb8b216e62
Lien881119 <Lien881119@gmail.com>
2022-12-29 23:31:42 +0800

    Update openmips.v

--- openmips.v
+++ openmips.v
@@ -94,10 +94,10 @@ module openmips (
                     .waddr(wb_wd_i), .wdata(wb_wdata_i), .re1(reg1_read),
                     .raddr1(reg1_addr), .rdata1(reg1_data),
                     .re2(reg2_read), .raadr2(reg2_addr), .rata2(read2_data)
-                    )
+                    );
     id_ex id_ex0(.clk(clk), .rst(rst),
                 //from id
                 .id_aluop(id_aluop_o), .id_alusel(id_alusel_o),
-                
-                )
+
+                );
 endmodule
