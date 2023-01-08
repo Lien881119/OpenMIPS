d5c0e8b9403dd709c34f8a973623911673905a2a
Lien881119 <Lien881119@gmail.com>
2023-01-08 16:52:43 +0800

    update logic and shift operation

--- ex.v
+++ ex.v
@@ -13,21 +13,52 @@ module ex (
     output reg wreg_o,
     output reg[`RegBus] wdata_o
 );
-    reg[`RegBus] logicout; //store result
+    reg[`RegBus] logicout; //store logic calculation result
+    reg[`RegBus] shiftres; //store shift result
 
     always @(*) begin
         if(rst == `RstEnable) begin
             logicout <= `ZeroWord;
         end else begin
             case (aluop_i)
-                `EXE_OR_OP:begin
+                `EXE_OR_OP: begin
                     logicout <= reg1_i | reg2_i;
                 end 
+                `EXE_AND_OP: begin
+                    logicout <= reg1_i & reg2_i;
+                end
+                `EXE_NOR_OP: begin
+                    logicout <= ~(reg1_i | reg2_i);
+                end
+                `EXE_XOR_OP: begin
+                    logicout <= reg1_i ^ reg2_i;
+                end
                 default: begin
                     logicout <= `ZeroWord;
                 end
             endcase
         end //end if
+    end //end always
+
+    always @(*) begin
+        if(rst == `RstEnable) begin
+            shiftres <= `ZeroWord;
+        end else begin
+            case (aluop_i)
+                `EXE_SLL_OP: begin
+                    shiftres <= reg2_i << reg1_i[4:0];
+                end
+                `EXE_SRL_OP: begin
+                    shiftres <= reg2_i >> reg1_i[4:0];
+                end
+                `EXE_SRA_OP: begin
+                    shiftres <= reg2_i ({32{reg2[31]}}<<(6'd32-{1'b0, reg1_i[4:0]})) | reg2_i >> reg1_i[4:0];
+                end                
+                default:begin
+                    shiftres <= `ZeroWord;
+                end 
+            endcase
+        end // end if
     end //end always
 
     always @(*) begin
@@ -37,6 +68,9 @@ module ex (
             `EXE_RES_LOGIC: begin
                 wdata_o <= logicout;
             end 
+            `EXE_RES_SHIFT: begin
+                wdata_o <= shiftres;
+            end
             default: begin
                 wdata_o <= `ZeroWord;
             end
